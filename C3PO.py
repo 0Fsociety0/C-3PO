f76fa94378a1d89373bf570a33dfa6f0d41720c4
0Fsociety0 <Fsociety0anonymous@gmail.com>
2019-07-14 23:33:49 -0700

    C3PO.py

--- C3PO.py
+++ C3PO.py
@@ -1,124 +1,64 @@
-import time
+import discord
 import json
+import time
 import aiohttp
 import asyncio
-import discord
 from discord import Game
-from discord.ext import commands
+from discord.ext.commands import Bot
 
-messages = joined = 0
 def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
 
-TOKEN = read_token()
+token = read_token()
 client = discord.Client()
-client = commands.Bot(command_prefix = './')
-
-async def update_stats():
-    await client.wait_until_ready()
-    global messages, joined
-
-    while not client.is_closed():
-        try:
-            with open("logs.txt", "a") as f:
-                f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")
-
-                messages = 0 
-                joined = 0
-
-                await asyncio.sleep(5)
-         except Exception as e:
-                print(e)
-                await asyncio.sleep(5)
 
 @client.event
-async def on_member_update(before, after):
-    n = after.nick
-    if n:
-        if n.lower().count("Eurphuct") > 0:
-            last = before.nick
-            if last:
-                await after.edit(nick=last)
-            else:
-                await after.edit(nick="Do not try that!")
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----------------')
+
+@client.event
+async def wait_until_login():
+    await client.change_presence(game=discord.Game(name="Looking for   Master"))
+    print("C-3PO is booting up." + cient.user.name)
+
+
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hello, I am C-3PO, human cyborg relations.")
 
 @client.event
 async def on_member_join(member):
     global joined
     joined += 1
-    for channel is member.server.channels:
-        if str(channel) == "general":
-            await channnel.send (f"""Welcome to the Galactic Republic {member.mention}""")
-
-
-@client.event
-async def on_message(message):
-    global messages
-    messages += 1
-
-    id = client.get_guild('fill this with ID')
-    channels = ["commands"]
-    valid_users = ["Eurphuct#3594"]
-    bad_words = ["Nigger", "Nig", "nigger", "nig", "Faggot", "fag"]
-
-    for word in bad_words:
-        if message.content.count(word) > 0:
-            print("Ah ah ahhhhh, that was naughty")
-            await message.channel.purge(limit=1)
-
-        if message.content == "./help":
-            embed = discord.Embed(title="Help on C-3PO", description="My commands"
-            embed.add_field(name="./hello", value="Greets the user")
-            embed.add_field(name="./users", value="Prints number of users")
-            embed.add_field(name="./bitcoin", value="Currency of bitcoin")
-            embed.add_field(name="./ping", value="Will tell you your ping")
-            await message.channel.send(content=None, embed=embed)
-
-        if str(message.channel) in channels and str(message.author) in valid_users:
-        if message.content.find("./hello") ./= -1:
-        await message.channel.send("Hello, I am C-3PO.  I am here to serve you.")
-        elif message.content == "./users":
-        await message.channel.send(f"""# of Members: {id.member_count}""")
-
-@client.event
-async def on_ready():
-    await client.change_presence(game=Game(name="with humans"))
-    print('C-3PO is booting up.')
+    for channel in member.server.channels:
+        if str(channel) == "575157257126281238":
+                print ("Welcome {member.mention} to the Galactic Republic.")
 
 @client.event
 async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name='Youngling')
-    await client.add_roles(member, role)
-
-@client.event
-async def on_member_join(member):
-    print("A new Youngling named " + member.name + " has joined the force.")
-    await client.send_message(member, newUserMessage)
-    print("sent message to " + member.name)
+    user = ctx.message.author
+    role = discord.utils.get(user.server.roles, name="Youngling")
+    await client.add_roles(user, role)
 
 @client.event
 async def on_member_remove(member):
-    print(member.name + "has left the Republic.  The Force has weakened")
-    
+    print(member.name + ", has left the Republic.  The Force has weakened")
+
 @client.event
 async def bitcoin():
-    url = 'https:///api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
     async with aiohttp.clientSession() as session:
         raw_response = await session.get(url)
-        response = await raw_response.text()
-        response = json.loads(response)
-        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
+        repsonse = await raw_response.next()
+        repsonse = json.loads(response)
+        await client.say("Bitcoin price: $" + response['bpi']['USD']['rate'])
 
+#Bot Commands Section ---------------------------------
 
-
-#Bot commands section
-
-@client.command
-async def ping(ctx):
-    await client.send(f'Pong! {round(Bot.latency * 1000}ms')
-
-
-client.loop.create_task(update_logs())
 client.run(token)
---
+++ README.txt
@@ -0,0 +1,16 @@
+		///////////////////////////////////
+		////				///
+		////	C-3PO Discord Bot	///
+		////				///
+		///////////////////////////////////
+
+This is a discord bot.  Currently working on the following:
+	1. Greeting new users upon joining / leaving in general chat.
+	2. Assigning role "Youngling" upon joining the server.
+	3. Direct Messaging the new user rules of the server and set role.
+	4. Allow users to pull Crypto-Currency value.
+
+
+	Future plans.
+	1. Have C-3PO intereact with R2-D2.
+	2. R2-D2 Music Bot.
