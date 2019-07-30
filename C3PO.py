b530a50d33bbe89ffe7ff19d48f18269c6dafac8
aykaym <aykayem92@gmail.com>
2019-07-29 21:34:10 -0400

    Made dice roller better for D&D

--- C3PO.py
+++ C3PO.py
@@ -35,6 +35,28 @@ def movie_exists(arg):
     return False
 
 
+def movies():
+    with open("watch_list", 'r') as movie_file:
+        head = [next(movie_file) for x in range(10)]
+    return head
+
+
+def rolling_dice(arg0, arg1):
+    i = 0
+    rolls = []
+    if arg0.isdigit() is False or arg1.isdigit() is False:
+        return "Thats not a number..."
+    elif int(arg1) > 10000 or int(arg1) > 10000:
+        return "Please use a number less than 10000"
+    else:
+        while i <= int(arg0):
+            number = random.randrange(0, int(arg1))
+            rolls.append(number)
+            i = i + 1
+    addition = sum(rolls)
+    average = sum(rolls) / len(rolls)
+    return ("Dice Rolls: " + str(rolls) + "\n" + "Analytics: \n" + "Sum: " + str(addition) + "\n" + "Average: " + str(average))
+
 
 #read_token, obviously
 def read_token():
@@ -84,16 +106,16 @@ async def hello(ctx):
 
 
 @client.command(pass_context=True, aliases =["Dice_roll","Dice_Roll", "DICE_ROLL"])
-async def dice_roll(ctx, arg):
+async def dice_roll(ctx, arg0, arg1):
     author = ctx.message.author.display_name
     channel = ctx.message.channel
-    if arg.isdigit() is False:
-        await channel.send("Thats not a number...")
-    elif int(arg) > 10000:
-        await channel.send("Please use a number less than 10000")
-    else:
-        number = random.randrange(0, int(arg))
-        await channel.send(author + " has rolled a " + str(number))
+    message = rolling_dice(arg0, arg1)
+    await channel.send(message)
+
+
+
+    #Make it better, handle more dice. Its going to be for DnD
+
 
 
 @client.command(pass_context=True, aliases = [])
@@ -109,10 +131,14 @@ async def suggestions(ctx, arg):
 
 
 @client.command(pass_context=True, aliases=[])
+@commands.has_role('TestRole')
 async def movie_list(ctx):
     author = ctx.message.author.display_name
     channel = ctx.message.channel
-    await channel.send("Not implemented")
+    await channel.send("Not Implemented")
+
+
+
 
 token = read_token()
 client.run(token, reconnect=True)
