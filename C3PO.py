f4c57ec3262a1f7b81add59fe80e40c399489fcb
ozyx <jessemazzella@gmail.com>
2019-08-18 12:46:35 -0700

    fix condition for rolling_dice

--- C3PO.py
+++ C3PO.py
@@ -41,7 +41,7 @@ def rolling_dice(arg0, arg1):
     rolls = []
     if arg0.isdigit() is False or arg1.isdigit() is False:
         return "Thats not a number..."
-    elif int(arg1) > 10000 or int(arg1) > 10000:
+    elif int(arg0) > 10000 or int(arg1) > 10000:
         return "Please use a number less than 10000"
     else:
         while i <= int(arg0):
